e0801036fbfda549d9d55c940408222b19566438
ghuy006 <144658457+ghuy006@users.noreply.github.com>
2023-09-11 22:30:46 +0700

    Add files via upload

--- paper.pas
+++ paper.pas
@@ -1,42 +1,32 @@
-var n,t,d,i,j,x,y:longint;
-    f1,f2:text;
-    a:array[1..1000000] of longint;
-    st1,st2,s1,s2:string;
-    code:integer;
-
+var     f1,f2:text;
+        n,i,j,t,k,kq:longint;
+        a:array[1..10000] of longint;
+        b:array[0..2] of longint;
+function min(x,y:longint):longint;
 begin
-assign(f1,'paper.inp'); reset(f1);
-assign(f2,'paper.out'); rewrite(f2);
-readln(f1,t);
-for j:=1 to t do
-        begin
-                d:=0;
-                readln(f1,n);
-                for i:=1 to n do
-                        read(f1,a[i]);
-                for i:=1 to n do
-                        begin
-                                 if i<>n then
-                                        begin
+        if x>y then min:=y else min:=x;
+end;
+begin
+        assign(f1,'paper.inp'); reset(f1);
+        assign(f2,'paper.out'); rewrite(f2);
+        readln(f1,t);
+        for i:=1 to t do
+                begin
+                        readln(f1,n);
+                        fillbyte(b,sizeof(b),0);
+                        for j:=1 to n do
+                                begin
+                                        read(f1,A[i]);
+                                        k:=a[i] mod 3;
+                                        inc(b[k]);
+                                end;
+                        if b[0] mod 2 =0 then
+                                                kq:= (b[0] div 2)+ min(b[1],b[2])
+                        else
+                                                kq:= ((b[0]-1) div 2 ) + min(b[1],b[2]);
+                        writeln(f2,kq);
+                end;
 
-                                                str(a[i],s1); str(a[i+1],s2);
-                                                st1:=s1+s2;
-                                                st2:=s2+s1 ;
-                                        end
-                                        else
-                                                begin
-                                                         str(a[n-1],s1); str(a[n],s2);
-                                                         st1:=s2+s1;
-                                                end;
-                                if st1<>st2 then
-                                        begin
-                                                val(st1,x,code); val(st2,y,code);
-                                                writeln(f2,x,' ',y);
-                                                if (x mod 3 = 0) or (y mod 3=0) and (x<>y) then inc(d);
-                                        end;
-                       end;
-
-                writeln(f2,d);
-        end;
-        close(f1); close(f2);
+        close(f1);
+        close(f2);
 end.
